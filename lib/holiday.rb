c8edd275ff080d7d09ca6ad9dcd08a463bd91305
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-12 02:34:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,9 +65,11 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  holiday_hash.map do |season, holidays|
+    holidays.each do |holiday, supplies|
+      supplies.include? "BBQ"
+    end
+  end.keys
 end
 
 
