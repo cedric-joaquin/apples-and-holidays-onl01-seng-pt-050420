9594afcc46e6d55b959c62789407c72f8d94d018
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-12 02:26:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,8 +41,10 @@ end
 
 
 def all_winter_holiday_supplies(holiday_hash)
-
-end
+  holiday_hash[:winter].map do |holiday, supplies|
+    supplies
+  end 
+end.flatten
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
