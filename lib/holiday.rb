e4b3b592830fc357108d921b6364733df900f4f9
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-12 02:37:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,9 +67,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
-      supplies.include? "BBQ"
+      holiday if supplies.include? "BBQ"
     end
-  end.keys
+  end
 end
 
 
